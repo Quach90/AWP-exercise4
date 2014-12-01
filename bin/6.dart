496f6734bee32bab3cd7213877c7c14b7365b8f9
Joakim Quach <joakimquach@gmail.com>
2014-12-01 16:59:37 +0100

    jh

--- bin/6.dart
+++ bin/6.dart
@@ -1,4 +1,12 @@
-import 'dart:html';
+//import 'dart:html';
+import 'package:http/http.dart' as http;
+
 void main() {
-  querySelector('#RipVanWinkle').text = 'Wake up, sleepy head!';
+
+  //querySelector('#RipVanWinkle').text = 'Wake up, sleepy head!';
+  var url = 'http://127.0.0.1:8080';
+  http.get(url).then((response) {
+    print("Response status: ${response.statusCode}");
+    print("Response body: ${response.body}");
+  });
 }
--- bin/Server.dart
+++ bin/Server.dart
@@ -1,17 +1,33 @@
 library simple_http_server;
 
 import 'dart:io';
-import 'package:http_server/http_server.dart' show VirtualDirectory;
+//import 'package:http_server/http_server.dart' show VirtualDirectory;
 
 void main() {
   final MY_HTTP_ROOT_PATH = Platform.script.resolve('Images').toFilePath();
   print(MY_HTTP_ROOT_PATH);
-  final virDir = new VirtualDirectory(MY_HTTP_ROOT_PATH)
-    ..allowDirectoryListing = true;
+  //final virDir = new VirtualDirectory(MY_HTTP_ROOT_PATH)
+    //..allowDirectoryListing = true;
+
+  final dir = new Directory(MY_HTTP_ROOT_PATH);
+  String test = "";
+  void addToTest(element){
+    test += element.path;
+  }
+
+
+  var contents = dir.listSync(recursive: true, followLinks: false).forEach(addToTest);
+
+
 
   HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080).then((server) {
-    server.listen((request) {
-      virDir.serveRequest(request);
+    server.listen((HttpRequest request) {
+
+      request.response..headers.contentType = new ContentType("text", "plain", charset: "utf-8")
+        ..write(test)
+        ..close();
+
+      //virDir.serveRequest(request);
     });
   });
 }
